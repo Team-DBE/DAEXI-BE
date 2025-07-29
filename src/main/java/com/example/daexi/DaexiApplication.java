1c76682a41ca8003fee548e26bd944b0bbae98c8
treegus <cowtree28@gmail.com>
2025-07-29 23:06:39 +0900

    2025-07-29 : 기본 설정
    프로젝트 build.gradle, application.yml 등을 설정

---
+++ src/main/java/com/example/daexi/DaexiApplication.java
@@ -0,0 +1,13 @@
+package com.example.daexi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaexiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaexiApplication.class, args);
+    }
+
+}
